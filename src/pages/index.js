74bbf8eb9d0a1ef2ff0747d05d025bcf75c37380
p-holodynski <p.holodynski@gmail.com>
2022-03-01 12:33:56 -1000

    Added contact form. Updated mobile design.

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,6 +25,21 @@ import ContactForm from "../components/contactForm"
 
 const IndexPage = props => (
   <div>
+    <header>
+      <a className="name" href="./">
+        PH
+      </a>
+      <div className="wrap">
+        <div className="icons">
+          <a href="https://github.com/p-holodynski" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="#8f3d4b"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+          </a>
+          <a href="https://www.linkedin.com/in/patryk-holodynski/" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="#8f3d4b"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+          </a>
+        </div>
+      </div>
+    </header>
     <ReactFullpage
       //fullpage options
       //Navigation
@@ -42,6 +57,8 @@ const IndexPage = props => (
       lazyLoading={true}
       //sectionsColor={["yellow", "orange", "#C0C0C0", "#ADD8E6"]}
       onLeave={(origin, destination, direction) => {
+        // This will return the width of the viewport
+        var intFrameWidth = window.innerWidth
         // Wrap every letter in a span
         var textWrapperFirstName = document.querySelector(
           ".ml9 .lettersFirstName"
@@ -102,7 +119,7 @@ const IndexPage = props => (
             "<span class='letter'>$&</span>"
           )
 
-        console.log("onLeave event", { origin, destination, direction })
+        // console.log("onLeave event", { origin, destination, direction })
         var background1 =
           "linear-gradient(to left, #ffbd8b, #ffbd8b, #7da3be, #7da3be, #7da3be)"
         var background2 =
@@ -111,11 +128,21 @@ const IndexPage = props => (
           "linear-gradient(to top left, #ffbd8b, #ffbd8b, #7da3be, #24344b, #24344b)"
         var background4 =
           "linear-gradient(to top left, #ffbd8b, #7da3be, #24344b, #24344b, #24344b)"
+        if (intFrameWidth <= 420) {
+          var background1 =
+            "linear-gradient(to bottom, #ffbd8b, #ffbd8b, #7da3be, #7da3be, #7da3be)"
+          var background2 =
+            "linear-gradient(to top, #ffbd8b, #ffbd8b, #7da3be, #7da3be, #24344b)"
+          var background3 =
+            "linear-gradient(to top, #ffbd8b, #ffbd8b, #7da3be, #24344b, #24344b)"
+          var background4 =
+            "linear-gradient(to top, #ffbd8b, #7da3be, #24344b, #24344b, #24344b)"
+        }
         if (destination.index !== 0) {
-          document.querySelector(".star").style.display = "block";
+          document.querySelector(".star").style.display = "block"
         }
         if (destination.index === 0) {
-          document.querySelector(".star").style.display = "none";
+          document.querySelector(".star").style.display = "none"
           anime
             .timeline({
               loop: false,
@@ -236,62 +263,53 @@ const IndexPage = props => (
               return 50 * i
             },
           })
-          // if(direction === "up") {
-          //   anime
-          //     .timeline({
-          //       loop: false,
-          //     })
-          //     .add(
-          //       {
-          //         targets: ".announcement .row .col div",
-          //         opacity: [1, 0],
-          //         easing: "easeOutCubic",
-          //         duration: 100,
-          //         delay: (el, i) => i * 100,
-          //       },
-          //       "0")
-          //   }
         }
         if (destination.index === 1) {
-          if(direction === "down") {
-          anime
-            .timeline({
-              loop: false,
-            })
-            .add({
-              targets: ".moonlight",
-              top: ["0", "75%"],
-              duration: 2000,
-              easing: "easeOutCubic",
-              delay: (el, i) => 45 * (i + 1),
-            })
-            .add({
-              targets: ".sky-color",
-              backgroundImage: [background1, background2],
-              duration: 100,
-              easing: "easeOutCubic",
-            }, "-=3000")
+          if (direction === "down") {
+            anime
+              .timeline({
+                loop: false,
+              })
+              .add({
+                targets: ".moonlight",
+                top: ["0", "75%"],
+                duration: 2000,
+                easing: "easeOutCubic",
+                delay: (el, i) => 45 * (i + 1),
+              })
+              .add(
+                {
+                  targets: ".sky-color",
+                  backgroundImage: [background1, background2],
+                  duration: 100,
+                  easing: "easeOutCubic",
+                },
+                "-=3000"
+              )
           }
 
-            if(direction === "up") {
-              anime
-                .timeline({
-                  loop: false,
-                })
-                .add({
-                  targets: ".moonlight",
-                  top: ["1000px", "75%"],
-                  easing: "easeOutCubic",
-                  duration: 2000,
-                  delay: (el, i) => 45 * (i + 1),
-                })
-                .add({
+          if (direction === "up") {
+            anime
+              .timeline({
+                loop: false,
+              })
+              .add({
+                targets: ".moonlight",
+                top: ["1000px", "75%"],
+                easing: "easeOutCubic",
+                duration: 2000,
+                delay: (el, i) => 45 * (i + 1),
+              })
+              .add(
+                {
                   targets: ".sky-color",
                   easing: "easeOutCubic",
                   backgroundImage: [background3, background2],
                   duration: 1000,
-                }, "-=3000")
-              }
+                },
+                "-=3000"
+              )
+          }
 
           anime({
             targets: ".js-letter",
@@ -346,6 +364,16 @@ const IndexPage = props => (
             })
             .add(
               {
+                targets: ".cover",
+                left: ["-100%", "100%"],
+                easing: "easeOutCubic",
+                duration: 2000,
+                delay: (el, i) => i * 200,
+              },
+              "-=400"
+            )
+            .add(
+              {
                 targets: ".header.skills.ml12 .letter",
                 translateX: [40, 0],
                 translateZ: 0,
@@ -354,17 +382,7 @@ const IndexPage = props => (
                 duration: 800,
                 delay: (el, i) => 100 + 30 * i,
               },
-              "-=400"
-            )
-            .add(
-              {
-                targets: ".cover",
-                left: ["-100%", "100%"],
-                easing: "easeOutCubic",
-                duration: 2000,
-                delay: (el, i) => i * 200,
-              },
-              "-=400"
+              "-=2200"
             )
             .add(
               {
@@ -390,27 +408,30 @@ const IndexPage = props => (
           })
         }
         if (destination.index === 2) {
-          if(direction === "down") {
-          anime
-            .timeline({
-              loop: false,
-            })
-            .add({
-              targets: ".moonlight",
-              top: ["75%", "1000px"],
-              easing: "easeOutCubic",
-              duration: 2000,
-              delay: (el, i) => 45 * (i + 1),
-            })
-            .add({
-              targets: ".sky-color",
-              easing: "easeOutCubic",
-              backgroundImage: [background2, background3],
-              duration: 1000,
-            }, "-=3000")
+          if (direction === "down") {
+            anime
+              .timeline({
+                loop: false,
+              })
+              .add({
+                targets: ".moonlight",
+                top: ["75%", "1000px"],
+                easing: "easeOutCubic",
+                duration: 2000,
+                delay: (el, i) => 45 * (i + 1),
+              })
+              .add(
+                {
+                  targets: ".sky-color",
+                  easing: "easeOutCubic",
+                  backgroundImage: [background2, background3],
+                  duration: 1000,
+                },
+                "-=3000"
+              )
           }
 
-          if(direction === "up") {
+          if (direction === "up") {
             anime
               .timeline({
                 loop: false,
@@ -422,13 +443,16 @@ const IndexPage = props => (
                 duration: 2000,
                 delay: (el, i) => 45 * (i + 1),
               })
-              .add({
-                targets: ".sky-color",
-                easing: "easeOutCubic",
-                backgroundImage: [background4, background3],
-                duration: 1000,
-              }, "-=3000")
-            }
+              .add(
+                {
+                  targets: ".sky-color",
+                  easing: "easeOutCubic",
+                  backgroundImage: [background4, background3],
+                  duration: 1000,
+                },
+                "-=3000"
+              )
+          }
 
           anime({
             targets: ".card.js-moon",
@@ -515,12 +539,15 @@ const IndexPage = props => (
               duration: 2000,
               delay: (el, i) => 45 * (i + 1),
             })
-            .add({
-              targets: ".sky-color",
-              easing: "easeOutCubic",
-              backgroundImage: [background3, background4],
-              duration: 1000,
-            }, "-=3000")
+            .add(
+              {
+                targets: ".sky-color",
+                easing: "easeOutCubic",
+                backgroundImage: [background3, background4],
+                duration: 1000,
+              },
+              "-=3000"
+            )
 
           anime({
             targets: ".blog-card.js-moon, .header.work.ml14",
@@ -551,6 +578,16 @@ const IndexPage = props => (
             })
             .add(
               {
+                targets: ".cover",
+                left: ["-100%", "100%"],
+                easing: "easeOutCubic",
+                duration: 2000,
+                delay: (el, i) => i * 200,
+              },
+              "-=400"
+            )
+            .add(
+              {
                 targets: ".header.contact.ml12 .letter",
                 translateX: [40, 0],
                 translateZ: 0,
@@ -559,12 +596,22 @@ const IndexPage = props => (
                 duration: 800,
                 delay: (el, i) => 100 + 30 * i,
               },
-              "-=400"
+              "-=1000"
+            )
+            .add(
+              {
+                targets: ".card.contact .announcement .wrapper",
+                opacity: ["0", "1"],
+                easing: "easeOutExpo",
+                duration: 1000,
+                delay: (el, i) => i * 200,
+              },
+              "-=800"
             )
         }
       }}
       render={({ state, fullpageApi }) => {
-        console.log("render prop change", state, fullpageApi) // eslint-disable-line no-console
+        //console.log("render prop change", state, fullpageApi) // eslint-disable-line no-console
         return (
           <ReactFullpage.Wrapper>
             <div className="section" id="scene">
@@ -880,7 +927,35 @@ const IndexPage = props => (
                     <div className="cover first"></div>
                     <div className="cover second"></div>
                     <div className="cover third"></div>
-                    <div className="wrapper">{/* <ContactForm /> */}</div>
+                    <div className="wrapper">
+                      <form
+                        name="contact-me-form"
+                        method="POST"
+                        data-netlify="true"
+                        className="contact-me"
+                      >
+                        <input
+                          type="text"
+                          name="name"
+                          placeholder="Your Name"
+                        />
+                        <input
+                          type="email"
+                          name="email"
+                          placeholder="Your Email"
+                        />
+                        <textarea
+                          name="message"
+                          placeholder="Your message"
+                        ></textarea>
+                        <input
+                          type="hidden"
+                          name="form-name"
+                          value="contact-me-form"
+                        />
+                        <button type="submit">Send</button>
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
